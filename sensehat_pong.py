6fa5c2bb6c7c013491fda0ca7367c70a3809c298
jboome363 <jboome363@lsr7.net>
2018-12-06 14:20:09 +0000

    more changes

--- sensehat_pong.py
+++ sensehat_pong.py
@@ -4,12 +4,19 @@ sense = SenseHat()
 print("Starting Pong...")
 white = (255, 255, 255)
 bat_y = 4
+#_____Functions_____
 #write more for draw_bat later
 def draw_bat():
+    sense.clear()
     sense.set_pixel(0, bat_y, white)
     sense.set_pixel(0, bat_y - 1, white)
     sense.set_pixel(0, bat_y + 1, white)
-#______Main program_______
-draw_bat()
-time.sleep(1)
-sense.clear()
+    time.sleep(0.25)
+def move_up(event):
+    global bat_y
+    if event.action == 'pressed':
+        bat_y -= 1
+#______Main program________
+sense.stick.direction_up = move_up
+while True:
+    draw_bat()
