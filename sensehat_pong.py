5550a2e2af163020578fec4144069e7be87f4d2e
jboome363 <jboome363@lsr7.net>
2018-12-06 14:09:14 +0000

    Start of the program, setting things up

--- sensehat_pong.py
+++ sensehat_pong.py
@@ -1,1 +1,15 @@
-print("Hello")
+from sense_hat import SenseHat
+import time
+sense = SenseHat()
+print("Starting Pong...")
+white = (255, 255, 255)
+bat_y = 4
+#write more for draw_bat later
+def draw_bat():
+    sense.set_pixel(0, bat_y, white)
+    sense.set_pixel(0, bat_y - 1, white)
+    sense.set_pixel(0, bat_y + 1, white)
+#______Main program_______
+draw_bat()
+time.sleep(1)
+sense.clear()
