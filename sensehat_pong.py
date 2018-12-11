2d4bb669855f9605aad2e6df3360a6c56b185030
jboome363 <jboome363@lsr7.net>
2018-12-11 08:10:01 -0600

    Added more essential mechanics. Many things are still broken, like the ball does not move correctly and the program will crash when it reaches the end of the screen, and collision with the bat.

--- sensehat_pong.py
+++ sensehat_pong.py
@@ -3,14 +3,23 @@ import time
 sense = SenseHat()
 print("Starting Pong...")
 white = (255, 255, 255)
-turquoise = (0, 255, 200)
+blue = (0, 0, 255)
 bat_y = 4
 ball_position = [3, 3]
 ball_velocity = [1, 1]
 #_____Functions_____
 def draw_ball():
-    sense.set_pixel(ball_position[0], ball_position[1], turquoise)
+    sense.set_pixel(ball_position[0], ball_position[1], blue)
     ball_position[0] += ball_velocity[0]
+    if ball_position[0] == 7:
+        ball_velocity[0] = -ball_velocity [0]
+    if ball_position[0] == 1 and ball_position[0] == bat_y:
+        ball_velocity[0] = -ball_velocity[0]
+    if ball_position[0] == 0:
+        sense.clear()
+        sense.clear(255, 0, 0)
+        time.sleep(9)
+    time.sleep(0.05)
 def draw_bat():
     sense.clear()
     sense.set_pixel(0, bat_y, white)
@@ -19,17 +28,22 @@ def draw_bat():
     time.sleep(0.25)
 def move_up(event):
     global bat_y
-    if event.action == 'pressed':
-        bat_y -= 1
+    if bat_y >= 2:
+        if event.action == 'pressed':
+            bat_y -= 1
 def move_down(event):
     global bat_y
-    if event.action == 'pressed':
-        bat_y += 1
+    if bat_y <= 5:
+        if event.action == 'pressed':
+            bat_y += 1
 #______Main program________
 sense.stick.direction_up = move_up
 sense.stick.direction_down = move_down
-while True:
-    draw_ball()
-    draw_bat()
 
+if ball_position[0] == 0:
+    ball_velocity[0] = -ball_velocity[0]
+while True:
+    draw_bat()
+    draw_ball()
+    sense.clear()
 
